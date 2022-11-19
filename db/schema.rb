8c162b578cc4a9947ffd6a6c15caee0559da80dd
Shyama Menon <shyama12@hotmail.com>
2022-11-19 13:06:17 +0100

    Added title column to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_104420) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_120434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_104420) do
     t.boolean "need_help"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["category_id"], name: "index_tasks_on_category_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
