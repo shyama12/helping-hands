e0054d356e0b03f13697e1aef7ab88a597ffb799
Seb Rojas <src.py@icloud.com>
2022-11-19 16:28:15 +0100

    initial map implementation

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :task_applications
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
---
+++ db/migrate/20221119150013_add_coordinates_to_tasks.rb
@@ -0,0 +1,6 @@
+class AddCoordinatesToTasks < ActiveRecord::Migration[7.0]
+  def change
+    add_column :tasks, :latitude, :float
+    add_column :tasks, :longitude, :float
+  end
+end
