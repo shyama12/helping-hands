a6f7e5b6ba75d6041fff733c989f45055e6e3115
Shyama Menon <shyama12@hotmail.com>
2022-12-01 13:24:03 +0100

    Seeds - destroy all messages and chatrooms to refresh DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts "Start seeds"
+Message.destroy_all
+Chatroom.destroy_all
 TaskApplication.destroy_all
 Task.destroy_all
 Category.destroy_all
