47abe418f2cdb28ca2f2204cd6ab83d3077f8d73
Shyama Menon <shyama12@hotmail.com>
2022-12-11 13:35:35 +0100

    Error pages and friendship bracelets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @tasks = Task.is_available_for_application.where(user: @user)
     @experiences = Experience.joins(task_application: :task).where(tasks: { user: @user })
+    @friendship_bracelets_count = @experiences.map(&:friendship_bracelet).size
+    puts "fb count #{@friendship_bracelets_count}"
   end
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,5 @@
 class Experience < ApplicationRecord
   belongs_to :task_application
+
+  default_scope { order(updated_at: :desc) }
 end
